980247548001e67ab85fe24b2525d3dc0938b90c
febeFlo <steffanyflorence@gmail.com>
2024-04-25 13:02:48 +0700

    Chapter 3 Sub 2

---
+++ 0706012210031-SteffanyFlorenceSM-AFL3/Views/PageView/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  0706012210031-SteffanyFlorenceSM-AFL3
+//
+//  Created by MacBook Pro on 25/04/24.
+//
+
+import Foundation
---
+++ 0706012210031-SteffanyFlorenceSM-AFL3/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSummary.swift
+//  0706012210031-SteffanyFlorenceSM-AFL3
+//
+//  Created by MacBook Pro on 25/04/24.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSummary()
+}
