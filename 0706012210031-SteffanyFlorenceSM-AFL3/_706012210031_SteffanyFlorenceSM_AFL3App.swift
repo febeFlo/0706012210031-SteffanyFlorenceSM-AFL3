473bc21b3c3ade01d1930bdcf01f5f39b012f278
febeFlo <steffanyflorence@gmail.com>
2024-05-03 09:23:39 +0700

    MacOS

--- 0706012210031-SteffanyFlorenceSM-AFL3/_706012210031_SteffanyFlorenceSM_AFL3App.swift
+++ 0706012210031-SteffanyFlorenceSM-AFL3/_706012210031_SteffanyFlorenceSM_AFL3App.swift
@@ -16,9 +16,20 @@ struct _706012210031_SteffanyFlorenceSM_AFL3App: App {
             ContentView()
                 .environment(modelData)
         }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
         
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
         #endif
     }
 }
--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -5,4 +5,35 @@
 //  Created by MacBook Pro on 03/05/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    // track the currently selected landmark
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
+    var body: some Commands {
+        SidebarCommands()
+        
+        // define the content menu
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" :  "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            // for automatically shows the keyboard shortcut in the menu
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+// read and write a custom property on the system-defined Focused Values structure
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
+    }
+}
