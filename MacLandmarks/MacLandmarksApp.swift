169bd72c5745605303acc2374504d9b1e3dedac2
febeFlo <steffanyflorence@gmail.com>
2024-05-03 08:18:17 +0700

    Watch OS

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by MacBook Pro on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
